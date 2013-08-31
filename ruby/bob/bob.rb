9070a9df1d7bc72923419f038b59da30f1b5f122
James Anderson <me@jamesaanderson.com>
2013-08-31 09:50:47 -0700

    Refactor ruby/bob

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,27 +1,28 @@
 class Bob
   def hey(msg)
-    if silence?(msg)
+    msg = Message.new(msg)
+    if msg.silence?
       'Fine. Be that way.'
-    elsif shouting?(msg)
+    elsif msg.shouting?
       'Woah, chill out!'
-    elsif question?(msg)
+    elsif msg.question?
       'Sure.'
     else
       'Whatever.'
     end
   end
+end
 
-  private
-
-  def question?(msg)
+class Message < Struct.new(:msg)
+  def question?
     msg.end_with?('?')
   end
 
-  def shouting?(msg)
+  def shouting?
     msg == msg.upcase
   end
 
-  def silence?(msg)
+  def silence?
     msg.to_s.empty?
   end
 end
